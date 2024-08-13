c35a1bc9b9019fdc0699c01484348de81ac218e4
soreloser010 <billie30042604@yumaunion.org>
2024-08-13 08:11:16 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const config = {
+const config =true pnpm start
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
